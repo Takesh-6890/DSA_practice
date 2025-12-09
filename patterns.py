cc7a0105ff2b5e70348b5cf0b72bceb807125ba8
aitktakesh <jtakesh007@gmail.com>
2025-12-09 16:56:08 +0530

    main

---
+++ patterns.py
@@ -0,0 +1,49 @@
+#simple horizontal line
+#n can taken from user by : n=int(input("enter the number"))
+n=5
+print("*"*n)
+#methond 2
+n=5
+for i in range(n):
+    print("*",end='')
+#simple vertical line
+n=5
+for i in range(n):
+    print("*")
+#square
+n=5
+for i in range(n):
+    for j in range(n):
+        print('*',end=' ')
+    print()
+#increasing triangle 
+n=5
+for i in range(n):
+    for j in range(i+1):
+        print('*',end=' ')
+    print()
+print(" ")
+#decreasing triangle
+n=5
+for i in range(n):
+    for j in range(i,n):
+        print('*',end=' ')
+    print()
+#right lower triangle
+print(" ")
+n=5
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for k in range(i+1):
+        print('*',end=" ")
+    print()
+#right upper triangle
+print("")
+n=5
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end=' ')
+    for k in range(i-1,n):
+        print('*',end=' ')
+    print()
