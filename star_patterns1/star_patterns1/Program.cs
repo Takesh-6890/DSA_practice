fe69638b29ea91acafb06215fef8260a64deb487
Takeshwar Jangita <takeshwarjangita@gmail.com>
2026-01-09 09:30:32 +0530

    Add files via upload

---
+++ star_patterns1/star_patterns1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+class StartPatterns1
+{
+    public static void Main(String[] args)
+    {
+        //right angle triangle
+        int n = 5;
+        for(int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=i;j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+
+        }
+
+        Console.WriteLine();
+        //inverted right angle
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                Console.Write("* ");
+
+            }
+            Console.WriteLine();
+        }
+        //pyramid
+        Console.WriteLine();
+        for(int i =1;i <=n;i++)
+        {
+            for (int s=1;s<=n-i;s++)
+            {
+                Console.Write("  ");
+            }
+            for(int j=1;j<=(2*i-1);j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+        //inverted Pyramid
+        Console.WriteLine();
+        for(int i=n;i>=1;i--)
+        {
+            for (int s = 1; s <= n - i; s++)
+            {
+                Console.Write("  ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Console.ReadLine();
+    }
+}
---
+++ start_patterns_3/start_patterns_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class StarPatterns3
+{
+    public static void Main(string[] args)
+    {
+        //hallowtraingle-right angled
+        int n = 5;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                if( j==0 || j==i || i==n-1)
+                {
+                    Console.Write("* ");
+
+                }
+                else
+                {
+                    Console.Write("  ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+
+
+    }
+}
